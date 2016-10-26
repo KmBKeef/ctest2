7c9c269d4be5bb76ba7bb76dcc7789d9bc180352
KmBKeef <mo.kefah@protonmail.com>
2016-10-26 19:36:08 +0300

    Add files via upload

---
+++ Cat.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+void speak() {
+	cout << "Meow" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+int main() {
+	
+	cout << "Hello World" << endl;
+
+	speak();
+		
+
+		
+	return 0;
+
+}
