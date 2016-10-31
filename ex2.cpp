f2bfe9d63f4cbdd8265adb3f05fc15458ffdfd26
KmBKeef <mo.kefah@protonmail.com>
2016-10-31 05:33:44 +0200

    Update ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -7,7 +7,7 @@ int main() {
 	int no;
 	
 	cout << "Enter numberes from 8 to 23 : " << flush;
-	
+	int a = 8;
 	for(int x = 8; x >= 8 && x <= 23;) {
 	
 	cin >> no;
@@ -23,7 +23,10 @@ int main() {
 
 	else {
 		cout << "Try again: " << flush;
-		
+		a++;
+		if(a=100) {
+		break;
+		}
 	}
 
 	}
